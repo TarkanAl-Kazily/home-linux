2059c642e6ac58b91a049f5860d4ee686cf4ea89
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2023-12-21 21:55:25 -0800

    [Neovim] Customize lualine and neotree
    * Disable lualine on neo-tree windows (since they're usually too small)
    * Switch fuzzy find and filter keybindings on neo_tree - / will now
      filter how I want it too (and <C-f> will revert the filter)

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,19 @@
+-- The status line at the bottom of the screen
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+        disabled_filetypes = {
+            statusline = {'neo-tree'},
+        },
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {'filename'},
+            lualine_x = {'encoding', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'searchcount', 'location'},
+        },
+    },
     config = true,
 }
